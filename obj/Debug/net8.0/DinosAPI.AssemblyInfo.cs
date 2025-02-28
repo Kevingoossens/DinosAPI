f842a587687880b4aeb409393cbe35fc8203d482
Kevingoossens <145009989+Kevingoossens@users.noreply.github.com>
2025-02-28 21:54:01 +0100

    dotnet run

--- obj/Debug/net8.0/DinosAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DinosAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DinosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94146aeb57420ea94aaf8508d8a33713a715548e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3de6adaa360a7c452b6b249f60a484e24bb8e23f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DinosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DinosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
